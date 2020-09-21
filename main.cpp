5f418b94d1a98239ea03ac59184973f8177eeac1
Wanixxx <46975493+Wanixxx@users.noreply.github.com>
2020-09-21 12:09:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop Go To*/
+/* run this program using the console pauser or add your own getch, system("pause") or input loop GoTo*/
 
 int main(int argc, char** argv) {
 	setlocale(LC_ALL, "Russian");
